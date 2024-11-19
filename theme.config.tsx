e5c943e3e91633a815deafff629337e94ec82567
Mahmud <55327257+Mahmud2640@users.noreply.github.com>
2024-11-20 00:59:29 +0600

    Add initial documentation and setup for NZTrip API with Tailwind CSS integration

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,37 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: (
+    <span style={{ color: "blue", fontSize: "40px", fontWeight: "bold" }}>
+      nztrip
+    </span>
+  ),
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/shuding/nextra-docs-template",
   },
   chat: {
-    link: 'https://discord.com',
+    link: "https://wa.me/01792492640?text=Hello%20I%20need%20help%20with%20the%20API",
+    icon: (
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg"
+        alt="WhatsApp"
+        style={{ height: "30px", width: "30px" }}
+      />
+    ),
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
   footer: {
-    text: 'Nextra Docs Template',
+    text: "NZTRIP API Docs",
   },
-}
+  banner: {
+    key: "1.0-release",
+    text: (
+      <a href="https://api.mynztrip.com" target="_blank">
+        🎉 nztrip 1.0 API is released. Read more →
+      </a>
+    ),
+  },
+  
+};
 
-export default config
+export default config;
